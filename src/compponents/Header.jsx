18d250c4eb0fe17a5d6a913bac2b3cfac8d4970a
Vlodeksus <volodymyrsus33@gmail.com>
2023-08-09 19:13:32 +0300

    Making Header component

---
+++ src/compponents/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import clases from './Header.module.css';
+
+const Header = () => {
+    return (
+        <div className={clases.header}>
+            header
+        </div>
+    );
+};
+
+export default Header;
